1f60b4526c6fefe1d3f296594ecf96f045ed8793
unknown <sniper6x@gmail.com>
2021-10-15 23:07:13 +0300

    simple-form-bot_webhooks

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: gunicorn --bind 0.0.0.0:8443 bot_app:app
+web: gunicorn --bind 127.0.0.1:8443 bot_app:app
--- bot_app.py
+++ bot_app.py
@@ -317,14 +317,14 @@ def get_message():
 
 
 # test_webhook_url = "https://9c55-31-40-108-124.ngrok.io"
-# tgm_bot_token = "2090254399:AAGn_Njw75I9szKUmPKN-T37_F3Y12hAf18"
-# heroku_app_name = "simple-form-bot-v1"
+tgm_bot_token = "2090254399:AAGn_Njw75I9szKUmPKN-T37_F3Y12hAf18"
+heroku_app_name = "simple-form-bot-v1"
 
 set_webhook_url_test_1 = "https://9c55-31-40-108-124.ngrok.io/2090254399:AAGn_Njw75I9szKUmPKN-T37_F3Y12hAf18/"
 set_webhook_url_heroku_1 = "https://simple-form-bot-v1.herokuapp.com/2090254399:AAGn_Njw75I9szKUmPKN-T37_F3Y12hAf18/"
 
 set_webhook_url_test = f"https://9c55-31-40-108-124.ngrok.io/{TGM_BOT_TOKEN}/"
-set_webhook_url_heroku = f"https://{HEROKU_APP_NAME}.herokuapp.com/{TGM_BOT_TOKEN}/"
+set_webhook_url_heroku = f"https://{heroku_app_name}.herokuapp.com/{tgm_bot_token}/"
 
 
 @app.route("/")
@@ -348,4 +348,4 @@ if __name__ == "__main__":
     # t.start()
     # port = int(os.environ.get("PORT", 8443))
 
-    app.run(host="0.0.0.0", port=8443, threaded=True, debug=True)
+    app.run(host="127.0.0.1", port=8443, threaded=True, debug=True)
